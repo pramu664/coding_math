d8a3ad4ae6c28ca7632f97dbc08c1afd017bf790
pramu664 <pramudithaayeshmantha0@gmail.com>
2023-09-15 14:13:31 +0530

    sin graph

--- trig/sin0.js
+++ trig/sin0.js
@@ -1,13 +1,33 @@
+// Draw a sin graph 
 window.onload = function() {
 
-	var canvas = document.getElementById("canvas"),
+	// Get the canvas and set up the context object for drawing 2D
+	let canvas = document.getElementById("canvas"),
 	ct = canvas.getContext("2d"),
+
+	// Set up and get the  the width and height of the canvas
 	width = canvas.width = window.innerWidth,
 	height = canvas.height = window.innerHeight,
-	centerX = width / 2,
-	centerY = height / 2;
 
-	ct.fillStyle = "green";
-	ct.fillRect(0, 0, width, height);
+	// X and Y axis of the graph scales
+	scaleX = 120,
+	scaleY = 120;
+
+	// Translate the origin of the coordinate system 
+	ct.translate(width/10, height/2);
+	// Reverse the y axis
+	ct.scale(1, -1)
+
+
+	// Increase the angle(radians) 
+	for(let angle = 0; angle < Math.PI * 2; angle += 0.01) {
+
+		// Get a point based on angle and sin value
+		let xP = angle * scaleX ,
+		yP = Math.sin(angle) * scaleY;
+
+		// Draw a small rectangle in that point 
+		ct.fillRect( xP, yP, 2, 2);
+	}
 
 }
