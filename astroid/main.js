14c7886e0d130520a1f2e5e552f1eaf97e7d43c0
pramu664 <pramudithaayeshmantha0@gmail.com>
2023-09-19 14:17:02 +0530

    Euler effect intergration

--- astroid/main.js
+++ astroid/main.js
@@ -4,26 +4,26 @@ window.onload = function () {
 	const width = canvas.width = window.innerWidth;
 	const height = canvas.height = window.innerHeight;
 
+	// Define acceleration vector 
+	const acceleration = vector.create(0.01, 0.01)
 
-	// Create hundred particles
-	const particles = [];
-	for (let i = 0; i < 100; i++) {
-		particles.push(particle.create(width/2, height/2, Math.random() * 5, Math.random() * Math.PI * 2));
-	}
+	const p = particle.create(0, height, 3, (3 * Math.PI/ 2));
+
+	
 
 	update();
 
 	function update() {
 
 		ct.clearRect(0, 0, width, height);
+		 ct.beginPath();
+		ct.arc(p.position.getX(), p.position.getY(), 10, 0, Math.PI * 2, false);
+		ct.fill();
 
-		// Draw particles 
-		for (let i = 0; i < particles.length; i++) {
-			ct.beginPath();
-			ct.arc(particles[i].position.getX(), particles[i].position.getY(), 10, 0, Math.PI * 2, false);
-			ct.fill();
-			particles[i].update();
-		}
+		// apply acceleration
+		p.velocity.addTo(acceleration);
+
+		p.update();
 
 		requestAnimationFrame(update);
 	}
