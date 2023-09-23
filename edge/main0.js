230c4066ebc58a9e31888ead9ef571b61b7ff1d1
pramu664 <pramudithaayeshmantha0@gmail.com>
2023-09-23 14:32:09 +0530

    implement smooth screen wrapping

--- edge/main0.js
+++ edge/main0.js
@@ -6,9 +6,10 @@ window.onload = function() {
 
 
 	const p = particle.create(100, height - 100, 4, Math.PI, 50);
+	const p2 = particle.create(100, height - 300, 4, 3*(Math.PI/2), 50);
 
 
-	//update();
+	update();
 
 
 	function update() {
@@ -16,22 +17,47 @@ window.onload = function() {
 
 		context.beginPath();
 		context.arc(p.position.getX(), p.position.getY(), p.radius, 0, Math.PI * 2, false);
+		context.arc(p2.position.getX(), p2.position.getY(), p2.radius, 0, Math.PI * 2, false);
 		context.fill();
 
 		p.update();
+		p2.update();
 
 
-	// TODO: Unfinished. make sure to add proper edge handling....
-		if (p.position.getX() - p.radius > width) {
-			p.position.setX(-p.radius);
-		}
-		if (p.position.getX() + p.radius < 0) {
-			p.position.setX(width + p.radius);
-		}
-		if (p.position.getY() - p.radius > height) {
-			p.position.setY(-p.radius);
+	// Blink effect edge handling is easy to understand. But it has draw backs. It's not realistic. But smooth version is good. It considering the "radius" of particle. 
+	// Imagine the point of rendering of the circle. If you add radius to it. The point move right. But first we want that point to be go left. so reduce the radius. That's the point we want to check. First figure out the point you want to check. Then you set the instantiating point. Again imagine the point. 0 point is not good it gives the blink effect. We don't want that. I am bad of documenting. Just view the results and understand.
+		if (p.position.getX() > width) {
+			p.position.setX(0);
+			// Blink effect
 		}
 
+		if (p.position.getX() < 0) {
+			p.position.setX(width);
+			// Blink effect
+		}
+
+
+		// X axis
+		if (p2.position.getX() - p2.radius > width) {
+			p2.position.setX(-p2.radius);
+			// Smooth effect
+		}
+
+		if (p2.position.getX() + p2.radius < 0) {
+			p2.position.setX(width + p2.radius);
+			// Smooth effect
+		}
+
+		// Y axis
+		if (p2.position.getY() - p2.radius > width) {
+			p2.position.setX(-p2.radius);
+			// Smooth effect
+		}
+
+		if (p2.position.getY() + p2.radius < 0) {
+			p2.position.setY(height + p2.radius);
+			// Smooth effect
+		}
 
 		requestAnimationFrame(update);
 	}
